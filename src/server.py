fe2b7846b37867f57b3743e487c65dc996350f17
Thomas Lundie <Lundie.T@liverpool.ac.uk>
2024-03-08 15:48:50 +0000

    Modified to add application level flow control

--- src/server.py
+++ src/server.py
@@ -1,18 +1,18 @@
 import socket
+import sys
 from threading import Thread
 
 class InvalidHeader(Exception):
     """The server sent an invalid header."""
 
-def extract_bits(byte, pos, count):
+
+def extract_bits(int_byte, pos, count):
     # Right shift the number by p-1 bits to get the desired bits at the rightmost end of the number
-    shifted_number = byte >> (8 - pos - 1)
- 
+    shifted_number = int_byte >> 8 - pos - count
+
     # Mask the rightmost k bits to get rid of any additional bits on the left
     mask = (1 << count) - 1
     return shifted_number & mask
-
-
 
     # END
 
@@ -20,36 +20,24 @@ TCP_IP = 'localhost'
 TCP_PORT = 6868
 BUFFER_SIZE = 1024
 
-""" tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-tcpsock.bind((TCP_IP, TCP_PORT))
-print("HI")
-tcpsock.listen(5)
-print("Howdy")
-c_sock, _ = tcpsock.accept()
-print("HI")
- """
 class ClientThread(Thread):
 
-    def __init__(self,ip,port,sock):
+    def __init__(self,ip,port,sock,handler):
         Thread.__init__(self)
         self.ip = ip
         self.port = port
         self.sock = sock
         #print " New thread started for "+ip+":"+str(port)
         self.flow = None
+        self.client_status = None
         self.data_type = None
         self.serialize_format = None
         self.encrypted = None
         self.transfer_size = None
+    
+    def process_operation_byte(self, op_byte):
 
-    def process_header(self, header):
-        # Process header
-        operation_byte, meta_byte = header[:2]
-
-        # Flow control
-        flow = extract_bits(operation_byte, 0, 4)
-        status = extract_bits(operation_byte, 4, 4)
+        flow = extract_bits(op_byte, 0, 3)
         if flow == 1:
             self.flow = "INIT"
         elif flow == 2:
@@ -59,25 +47,41 @@ class ClientThread(Thread):
         else:
             raise InvalidHeader("Flow control")
         
-        if self.flow == "INIT":
-            # INIT
-            self.data_type = extract_bits(meta_byte, 0, 3)
-            self.serialize_format = extract_bits(meta_byte, 3, 3)
-            self.encrypted = (meta_byte >> 7) & 1
-            self.transfer_size = int.from_bytes(header[2:4], "little")
+        status = extract_bits(op_byte, 3, 3)
+        if status == 0:
+            self.client_status = "OK"
+        else:
+            self.client_status = "ERROR"
+
+        self.final = (op_byte >> 1) & 1
+
+    def process_header(self, header):
+        # Process header
+        operation_byte, meta_byte = header[:2]
+        # Operation byte controls flow of data transfer
+        self.process_operation_byte(operation_byte)
+        if self.flow != "END":
+            self.transfer_size = int.from_bytes(header[2:], "big")
         
-        elif self.flow == "CONT":
-            # CONT
-            self.transfer_size = int.from_bytes(header[2:4], "little")
+            if self.flow == "INIT":
+                # INIT
+                self.data_type = extract_bits(meta_byte, 0, 3)
+                self.serialize_format = extract_bits(meta_byte, 3, 3)
+                self.encrypted = (meta_byte >> 1) & 1
+                self.transfer_size = int.from_bytes(header[2:], "big")
+            
+            elif self.flow == "CONT":
+                # CONT
+                self.transfer_size = int.from_bytes(header[2:], "big")
     
     def run(self):
         while True:
             try:
-                self.packet_iteration()
+                self.session_controller()
             except InvalidHeader:
                 pass
 
-    def packet_iteration(self):
+    def session_controller(self):
         header = bytes()
         payload = bytes()
         remaining_header = 4
@@ -95,16 +99,50 @@ class ClientThread(Thread):
             self.sock.close()
             return False
         
-        while True:
-            buffer = self.sock.recv(1024)
-            if not buffer:
+        while len(payload) < self.transfer_size:
+            buffer = self.sock.recv(1024,  socket.MSG_WAITALL)
+            if buffer:
                 payload += buffer
-                if len(payload) == self.transfer_size:
-                    break
         
         if self.data_type == 0:
             # Text File
-            print(repr(payload))
+            if self.serialize_format == 0:
+                handler.write(buffer.decode("utf-8"))
+        
+        handler.flush()
+
+server_mode = "PRINT"
+server_write = "CONTINUOUS"
+# sever_mode = "SAVE"
+
+class UploadHandler():
+    
+    def __init__(self):
+        self.buffer = ""
+        if server_write == "END":
+            self.continuous = False
+        elif server_write == "CONTINUOUS":
+            self.continuous = True
+
+        if server_mode == "PRINT":
+            self.stream = sys.stdout
+        else:
+            self.stream = open("C:\Temp\hello.txt", "w")
+    
+    def write(self, string):
+        if self.continuous:
+            self.stream.write(string)
+        else:
+            self.buffer += string
+    
+    def flush(self):
+        if self.continuous:
+            self.stream.flush()
+        else:
+            self.stream.write(self.buffer)
+            self.stream.flush()
+
+handler = UploadHandler()
 
 tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -116,7 +154,7 @@ while True:
     #print "Waiting for incoming connections..."
     (conn, (ip,port)) = tcpsock.accept()
     #print 'Got connection from ', (ip,port)
-    newthread = ClientThread(ip,port,conn)
+    newthread = ClientThread(ip,port,conn,handler)
     newthread.start()
     threads.append(newthread)
 
